b044fb82d89cb28d95397ca590bc2abc27222086
hallmark1984 <matt.mcaleavy@gmail.com>
2026-03-16 23:42:35 +0000

    v2

--- startup.sh
+++ startup.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-# Move to the directory where the repo is cloned
-cd "$(dirname "$0")"
+cd /home/pi/media-stack
 
-# Update from Git
+# Force mount if not present
+sudo mount -a
+
+# Ensure the 'pi' user owns the data folder for Samba/Apps
+sudo chown -R pi:pi ./data
+
 git pull origin main
-
-# Force create necessary folders if they got deleted
-mkdir -p data/media/tv data/media/audiobooks data/media/podcasts data/torrents config/sonarr config/qbittorrent config/audiobookshelf config/postgres
-
-# IMPORTANT: Fix permissions for the 'pi' user so Docker doesn't fail
-sudo chown -R $USER:$USER .
-sudo chmod -R 775 .
-
-# Fire up the containers
 docker compose up -d --remove-orphans
